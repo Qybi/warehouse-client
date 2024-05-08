d6fc9307ca402200085c93cd5f035e1e287e53d8
Luca Bernardis <lucabusinesswork1@gmail.com>
2024-05-08 11:29:02 +0200

    modifiche sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    { path: '/home', pathMatch: 'full', component: HomeComponent },
+    { path: 'home', pathMatch: 'full', component: HomeComponent },
     { path: '*', pathMatch: 'full', component: HomeComponent }
 ];
--- src/app/wrapper/wrapper.component.ts
+++ src/app/wrapper/wrapper.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-wrapper',
   standalone: true,
-  imports: [NavbarComponent, SidebarComponent],
+  imports: [NavbarComponent, SidebarComponent, RouterOutlet],
   templateUrl: './wrapper.component.html',
   styleUrl: './wrapper.component.scss'
 })
