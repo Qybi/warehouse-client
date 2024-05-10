2891f0db740230deb11caf33b50729e30f19b4a0
Luca Bernardis <lucabusinesswork1@gmail.com>
2024-05-10 11:14:56 +0200

    install ng-bootstrap

--- src/app/wrapper/wrapper.component.ts
+++ src/app/wrapper/wrapper.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
+import { TableComponent } from '../components/table/table.component';
 
 
 @Component({
   selector: 'app-wrapper',
   standalone: true,
-  imports: [NavbarComponent, SidebarComponent, RouterOutlet],
+  imports: [NavbarComponent, SidebarComponent, RouterOutlet, TableComponent],
   templateUrl: './wrapper.component.html',
   styleUrl: './wrapper.component.scss'
 })
