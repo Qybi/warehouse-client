dfb3870e7cce72f8c061bef607014f6b48000ee9
Raffaele <raffaele.trivillin@gmail.com>
2024-05-17 15:59:56 +0200

    fixed headers concat and added date split utility

---
+++ src/app/useful-utilities.spec.ts
@@ -0,0 +1,7 @@
+import { UsefulUtilities } from './useful-utilities';
+
+describe('UsefulUtilities', () => {
+  it('should create an instance', () => {
+    expect(new UsefulUtilities()).toBeTruthy();
+  });
+});
