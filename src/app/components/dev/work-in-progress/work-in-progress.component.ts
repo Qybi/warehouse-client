fd699dea3e5c0c1eb22b971d1466ea9b3f163c81
Raffaele <raffaele.trivillin@gmail.com>
2024-05-18 18:34:53 +0200

    created a work in progress component

---
+++ src/app/components/dev/work-in-progress/work-in-progress.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-in-progress',
+  standalone: true,
+  imports: [],
+  templateUrl: './work-in-progress.component.html',
+  styleUrl: './work-in-progress.component.scss'
+})
+export class WorkInProgressComponent {
+
+}
