275c167c6a587dae303011072aad7298d6d694e9
Raffaele <raffaele.trivillin@gmail.com>
2024-05-18 18:37:39 +0200

    added stock manager and creation modal + routing

---
+++ src/app/components/stocks-manager/modal-create-stock/modal-create-stock.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-create-stock',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-create-stock.component.html',
+  styleUrl: './modal-create-stock.component.scss'
+})
+export class ModalCreateStockComponent {
+
+}
