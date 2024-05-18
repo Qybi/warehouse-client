43e50dd87df2fd4ae1eeb70a759f8ac5ac267896
Raffaele <raffaele.trivillin@gmail.com>
2024-05-18 18:35:35 +0200

    declared assign bundle as WIP

--- src/app/components/modals/shared/modal-assign-bundle/modal-assign-bundle.component.ts
+++ src/app/components/modals/shared/modal-assign-bundle/modal-assign-bundle.component.ts
@@ -3,16 +3,18 @@ import { Student } from '../../../../models/student';
 import { Pc } from '../../../../models/pc';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { PCService } from '../../../../services/pc.service';
+import { StudentView } from '../../../../models/views/student-view';
+import { WorkInProgressComponent } from '../../../dev/work-in-progress/work-in-progress.component';
 
 @Component({
   selector: 'app-modal-assign-bundle',
   standalone: true,
-  imports: [],
+  imports: [WorkInProgressComponent],
   templateUrl: './modal-assign-bundle.component.html',
   styleUrl: './modal-assign-bundle.component.scss'
 })
 export class ModalAssignBundleComponent {
-  student: Student = {} as Student
+  student: StudentView = {} as StudentView
   pc: Pc = {} as Pc
 
   constructor(
@@ -20,8 +22,7 @@ export class ModalAssignBundleComponent {
     private pcservice: PCService
   ) { }
 
-  initModal(student: Student) {
+  initModal(student: StudentView) {
     this.student = student;
-
   }
 }
