59a99e9a12084094aad65278a023085baa909217
Luca Bernardis <lucabusinesswork1@gmail.com>
2024-05-19 17:20:59 +0200

    merging Branch

---
+++ src/app/models/accessory-assignment.ts
@@ -0,0 +1,21 @@
+import { Accessory } from "./accessory";
+import { ReasonsAssignment } from "./reasons-assignment";
+import { ReasonsReturn } from "./reasons-return";
+import { Student } from "./student";
+
+export interface AccessoryAssignment {
+  id: number;
+  accessoryId: number;
+  studentId: number;
+  assignmentDate: string;
+  assignmentReasonId: number;
+  isReturned: boolean;
+  forecastedReturnDate: string;
+  actualReturnDate: string;
+  returnReasonId: number;
+
+  accessory: Accessory;
+  student: Student;
+  assignmentReason?: ReasonsAssignment;
+  returnReason?: ReasonsReturn;
+}
